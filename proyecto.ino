15496c3fed8b9b2903db63543cb0f6807dbcb215
Luis Castro <luisarmandocastro12@gmail.com>
2022-11-24 23:41:37 -0700

    Se agrego conexion firebase y sensor de humedad dinamico

--- proyecto.ino
+++ proyecto.ino
@@ -1,31 +1,102 @@
 #include <WiFiClientSecure.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <Firebase_ESP_Client.h>
 
 const char* ssid = "Totalplay-C5A1";
 const char* password = "C5A163D3E3PAe6Rw";
 
+#include "addons/TokenHelper.h"
+#define API_KEY "AIzaSyCkEVunp97TM5RYZXri-9EwuWb6L722XZk";
+
+#define USER_EMAIL "micontra@gmail.com";
+#define USER_PASSWORD "micontra";
+
+#define DATABASE_URL "https://login-glam.firebaseio.com/";
+
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+
 
 WiFiClientSecure client;
+const int Sensor_pin = 32;
+float Sensor_value;
 
+String humPath;
+
+
+float Humedad=0;
 void setup() {
   Serial.begin(115200);
-  connectToWiFi();
 
-  while (WiFi.status() == WL_CONNECTED) {
-    Serial.println("");
-    Serial.print("peticion");
-    // wait 1 second for re-trying
-    char peti = petition("https://pokeapi.co/api/v2/pokemon/ditto", "GET", "");
-    delay(2000);
-  }
+  connectToWiFi();
+   
+
+
 }
 
 void loop() {
-
+  if (WiFi.status() == WL_CONNECTED) {
+   
+    Sensor_value = getDataHumidity();
+    Serial.print("Soil Moisture is  = ");
+    Serial.print(Sensor_value);
+    sendFloat("/humedad", Sensor_value);
+    Serial.println("%");
+    delay(3000);
+  }
+}
+void sendFloat(String path, float value){
+  if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), value)){
+    Serial.print("Writing value: ");
+    Serial.print (value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
 }
 
+void connectFirebase(){
+   // Assign the api key (required)
+  config.api_key = API_KEY;
 
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+}
+
+float getDataHumidity() {
+  Serial.println("Row data ---> " + String(analogRead(Sensor_pin)));
+
+  Humedad = 100.0 - (analogRead(Sensor_pin)/4096.0) * 100.0;
+  
+  return Humedad;
+
+}
 void connectToWiFi() {
   //Initialize serial and wait for port to open:
   delay(100);
@@ -39,63 +110,9 @@ void connectToWiFi() {
     // wait 1 second for re-trying
     delay(1000);
   }
+  connectFirebase();
   // attempt to connect to Wifi network:
   Serial.print("Connected to ");
   Serial.println(ssid);
-
-}
-
-//GET, POST, PATCH, PUT, DELETE
-void petition(char * serverName, char * metodo, char * body) {
-
-  HTTPClient http;
-
-  char * payload = "";
-
-  // Your Domain name with URL path or IP address with path
-  http.begin(serverName.c_str());
-
-  // If you need Node-RED/server authentication, insert user and password below
-  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-
-  // Send HTTP GET request
-  int httpResponseCode = 0;
-  if (metodo != 'GET' || metodo != 'DELETE' ) {
-    http.addHeader("Content-Type", "application/json");
-  }
-
-  switch (metodo) {
-    case ‘GET’:
-      httpResponseCode = http.GET();
-                         break;
-    case ‘POST’:
-      httpResponseCode = http.POST(body);
-                         break;
-    case ‘PATCH’:
-      httpResponseCode = http.PATCH(body);
-                         break;
-    case ‘PUT’:
-      httpResponseCode = http.PUT(body);
-                         break;
-    case ‘DELETE’:
-      httpResponseCode = http.DELETE();
-                         break;
-  }
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println("HTTPResponseCode" + httpResponseCode);
-    payload = http.getString();
-
-  } else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-    payload = 'ERROR ' + httpResponseCode
-  }
-  // Free resources
-  http.end();
-  return payload
-}
-
-
 
 }
